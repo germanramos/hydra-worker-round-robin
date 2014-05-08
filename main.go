6cbae5f27bb8d269ee08b303b0ffd0ccb978e46e
Alberto Gregoris <alberto.gregoris@intelygenz.com>
2014-05-08 18:04:31 +0200

    Worker tested and ready for alpha

--- main.go
+++ main.go
@@ -1,18 +1,31 @@
 package main
 
 import (
-	"encoding/json"
+	"math/rand"
 	"os"
+	"time"
 
-	worker "github.com/innotech/hydra-worker-pong/vendors/github.com/innotech/hydra-worker-lib"
+	worker "github.com/innotech/hydra-worker-round-robin/vendors/github.com/innotech/hydra-worker-lib"
 )
 
-var lastInstanceIndex int = 0
+var lastInstanceIndex map[string][]int = make(map[string][]int)
 
-func sortSlice(fisrtElement int) []interface{} {
+func sortSlice(instances []interface{}, fisrtElement int, appId string, iteration int) []interface{} {
+	var index int = lastInstanceIndex[appId][iteration]
 	computedInstances := make([]interface{}, 0)
-	computedInstances = append(computedInstances, instances[lastInstanceIndex+1:])
-	computedInstances = append(computedInstances, instances[:lastInstanceIndex+1])
+	if index < len(instances) {
+		computedInstances = append(computedInstances, instances[index:])
+	}
+	if index > 0 {
+		computedInstances = append(computedInstances, instances[:index])
+	}
+
+	if index < len(instances)-1 {
+		lastInstanceIndex[appId][iteration] = lastInstanceIndex[appId][iteration] + 1
+	} else {
+		lastInstanceIndex[appId][iteration] = 0
+	}
+
 	return computedInstances
 }
 
@@ -25,21 +38,46 @@ func main() {
 	verbose := len(os.Args) >= 4 && os.Args[3] == "-v"
 
 	// New Worker connected to Hydra Load Balancer
-	mapAndSortWorker := worker.NewWorker(serverAddr, serviceName, verbose)
-	fn := func(instances []map[string]interface{}, args map[string]string) []interface{} {
+	roundRobinWorker := worker.NewWorker(serverAddr, serviceName, verbose)
+	fn := func(instances []interface{}, args map[string]interface{}) []interface{} {
 		var computedInstances []interface{}
 
-		if len(instances) > lastInstanceIndex {
-			if len(instances) > lastInstanceIndex+1 {
-				computedInstances = sortSlice(lastInstanceIndex + 1)
-			}
+		appId := args["appId"].(string)
+		iteration := args["iteration"].(int)
+		if _, ok := lastInstanceIndex[appId]; !ok {
+			lastInstanceIndex[appId] = make([]int, 0)
+		}
+		if iteration >= len(lastInstanceIndex[appId]) {
+			lastInstanceIndex[appId] = append(lastInstanceIndex[appId], 0)
+		}
+		var index int = lastInstanceIndex[appId][iteration]
+		if len(instances) > index {
+			computedInstances = sortSlice(instances, index+1, appId, iteration)
 		} else {
 			rand.Seed(time.Now().Unix())
 			randomIndex := rand.Intn(len(instances))
-			computedInstances = sortSlice(randomIndex)
+			computedInstances = sortSlice(instances, randomIndex, appId, iteration)
 		}
 
 		return computedInstances
 	}
-	mapAndSortWorker.Run(fn)
+	roundRobinWorker.Run(fn)
+
+	// roundRobinWorker := worker.NewWorker(serverAddr, serviceName, verbose)
+	// fn := func(instances []map[string]interface{}, args map[string]string) []interface{} {
+	// 	var computedInstances []interface{}
+
+	// 	if len(instances) > lastInstanceIndex {
+	// 		if len(instances) > lastInstanceIndex+1 {
+	// 			computedInstances = sortSlice(lastInstanceIndex + 1)
+	// 		}
+	// 	} else {
+	// 		rand.Seed(time.Now().Unix())
+	// 		randomIndex := rand.Intn(len(instances))
+	// 		computedInstances = sortSlice(randomIndex)
+	// 	}
+
+	// 	return computedInstances
+	// }
+	// roundRobinWorker.Run(fn)
 }
